08851d71225e5fd565ae70d76cb1c2d25fb9e4f4
Juric Talovirov <jurictalovirov@gmail.com>
2016-03-29 16:13:47 +0300

    Исправленны всплывающие подсказки, исправленна кнопка Help

--- GoFrontend/Tech Skills/JS 7-8/js/script.js
+++ GoFrontend/Tech Skills/JS 7-8/js/script.js
@@ -1,44 +1,82 @@
 ;$(function() {
+      var timersTitleHide = [];
 
-		$('.tabs-item').each(function(i){
-			$(this).attr('data-tab', 'tab' + i);
-		});
-		$('.tabs-content').each(function(i){
-			$(this).attr('data-tab', 'tab' + i);
-		});
+      $('.tabs-item').each(function(i){
+         $(this).attr('data-tab', 'tab' + i);
+      });
+      $('.tabs-content').each(function(i){
+         $(this).attr('data-tab', 'tab' + i);
+      });
+
+      $('.form-input').each(function(i){
+         var $tooltip;
+         var $position;
+         var $form = $(this).closest('.form');
+
+         if ($(this).data('title')) {
+            $tooltip = $('<div class="tooltip" data-tooltip-id="' + i + '"></div>');
+            $tooltip.text($(this).data('title'));
+            $tooltip.appendTo($(this).closest('.form'));
+            $(this).attr('data-tooltip-id', i);
+
+            $position = $(this).offset();
+            $position.left += $(this).width() + 25;
+
+            $tooltip.offset($position);
+         }
+      });
+
 
    $('.tabs-item').on('click', function(){
-   	var $getTabs = $(this).closest('.tabs');
-   	var $dataTab = $(this).data('tab');
+      var $getTabs = $(this).closest('.tabs');
+      var $dataTab = $(this).data('tab');
 
-   	$getTabs.find('.tabs-item').removeClass('tabs-item--active');
-   	$(this).addClass('tabs-item--active');
+      $getTabs.find('.tabs-item').removeClass('tabs-item--active');
+      $(this).addClass('tabs-item--active');
 
-   	$getTabs.find('.tabs-content').hide();
-   	$getTabs.find('div[data-tab=' + $dataTab + ']').show();
+      $getTabs.find('.tabs-content').hide();
+      $getTabs.find('div[data-tab=' + $dataTab + ']').show();
    });
 
    // Tooltip
    $('.form .form-input').hover(showTooltip, hideTooltip);
    $('.form .tooltip').hover(showTooltip, hideTooltip);
-});
+
+   //button
+   $('.button').on({
+      mousedown: function(){
+         var $form = $(this).closest('.form');
+
+         $(this).addClass('button-click');
+         $(this).closest('.form').find('.tooltip').show();
+      },
+      mouseup: function(){
+         $(this).removeClass('button-click');
+      } 
+   });
 
 function showTooltip(){
-      var $position = $(this).offset();
-      var $form = $(this).closest('.form');
-      var $tooltip = $form.find('.tooltip');
-      var $text = $(this).data('title');
+   var $form = $(this).closest('.form');
+   var tooltipID = $(this).data('tooltip-id');
+   var $tooltip = $form.find('div[data-tooltip-id=' + tooltipID + ']');
 
-      $position.left += $(this).width() + 25;
+   if (timersTitleHide[tooltipID]) clearTimeout(timersTitleHide[tooltipID]);
 
-      $tooltip.text($text);
-      $tooltip.show();
-      $tooltip.offset($position);
+   $tooltip.show();
 };
 
 function hideTooltip(){
-   var timeout1 = setTimeout(function(elem){
-      elem.closest('.form').find('.tooltip').hide();
-   }($(this)), 500);
+   var $that = $(this);
+   var tooltipID = $that.data('tooltip-id');
+   console.log('timers[' + tooltipID + '] hideTooltip()');
+   
+   timersTitleHide[tooltipID] = setTimeout(function(){
+      callback($that);
+   }, 800);
 
+   function callback(elem){
+      elem.closest('.form').find('div[data-tooltip-id=' + tooltipID + ']').hide();
+   }
 };
+
+});

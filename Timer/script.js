fce30f4d27dfdbb819f85cd1e4e6db371eb94683
Juric Talovirov <jurictalovirov@gmail.com>
2016-07-02 17:21:32 +0300

    oop

--- Timer/script.js
+++ Timer/script.js
@@ -1,121 +1,183 @@
-;(function(){
+//===================
+
 var timer = new Timer();
-var clockface = document.querySelector('.timer-clockface');
-var startPauseButton = document.querySelector('.start-pause');
-var clearButton = document.querySelector('.clear');
-var timerEvent;
-var START_BUTTON_TEXT = 'Start';
-var PAUSE_BUTTON_TEXT = 'Pause';
-var timeStart;
-var pause = 0;
+var v = new View();
+var c = new Control(timer,v);
 
-startPauseButton.addEventListener('click', startPauseClick);//Клик кнопки Запуск/пауза таймера
-clearButton.addEventListener('click', clearButtonClick);//Клик кнопки Сброс таймера
+//==================
 
-function startPauseClick(){
-	if (startPauseButton.innerHTML === START_BUTTON_TEXT) {
-		timeStart = Date.now();
-		timerEvent = setInterval(timerRun, 1);
-		startPauseButton.innerHTML = PAUSE_BUTTON_TEXT;
-	}else {
-		clearInterval(timerEvent);
-		pause = Date.now() - timeStart + pause;
-		startPauseButton.innerHTML = START_BUTTON_TEXT;
+function Timer() {
+	var self = this;
+
+	this.init = function () {
+		self.currentTime = self.pauseTime = self.startTime = 0;
+		self.hour = self.min = self.sec = self.msec = 0;
+	}
+
+	self.init();
+
+	this.getTime = function () {
+		return self.currentTime;
+	}
+
+	this.start = function (callback) {
+		self.startTime = Date.now();
+
+		self.timerName = setInterval(timerRun, 1);
+
+		function timerRun(){
+			var interval = Date.now() - self.startTime + self.pauseTime;// pause
+			var H = 60 * 60 * 1000;
+			var M = 60 * 1000;
+			var S = 1000;
+
+			self.hour = (interval / H) >> 0;
+			interval = interval % H;
+
+			self.min = (interval / M) >> 0;
+			interval = interval % M;
+
+			self.sec = (interval / S) >> 0;
+			interval = interval % S;
+
+			self.msec = interval;
+
+			(callback)(self.toString());
+
+		}
+	};
+
+	this.stop = function () {
+		clearInterval(self.timerName);
+		self.timerName = undefined;
+		self.pauseTime = Date.now() - self.startTime + self.pauseTime;
+	};
+
+	this.reset = function () {
+		self.stop();
+		self.init();
+	};
+
+	this.getHour = function () {
+		return self.hour
+	}
+
+	this.getMin = function () {
+		return self.min
+	}
+
+	this.getSec = function () {
+		return self.sec
+	}
+
+	this.getMSec = function () {
+		return self.msec;
+	}
+
+	this.toString = function () {
+		var outString = '';
+	
+		if (self.hour < 10) outString += '0' + self.hour
+		else outString += self.hour + '';
+		outString += ':';
+		
+		if (self.min < 10) outString += '0' + self.min
+		else outString += self.min + '';
+		outString += ':';
+
+		if (self.sec < 10) outString += '0' + self.sec
+		else outString += self.sec + '';
+		outString += '.';
+
+		if (self.msec > 99) outString += self.msec + '';
+		if ((self.msec < 100)&&(self.msec > 9)) outString += '0' + self.msec;
+		if (self.msec < 10) outString += '00' + self.msec;
+
+		return outString;
+	}
+}//Timer
+
+function View() {
+	var self = this;
+
+	self.counter = new Counter();
+
+	self.elements = {
+		timerContainer   : document.querySelector('.timer'),
+		clockface        : document.querySelector('.timer').querySelector('.timer-clockface'),
+		timerList        : document.querySelector('.timer').querySelector('.timer-list'),
+		startPauseButton : document.querySelector('.timer-startStop'),
+		splitButton      : document.querySelector('.timer-Split'),
+		clearButton      : document.querySelector('.timer-Reset'),
+	};
+
+	this.start = function () {
+		self.elements.startPauseButton.innerHTML = 'Stop';
+	}
+
+	this.stop = function () {
+		self.elements.startPauseButton.innerHTML = 'Start';
+		self.addSplit('Stop');
+	}
+
+	this.reset = function () {
+		self.clearSplitList();
+		self.elements.startPauseButton.innerHTML = 'Start';
+		self.displayingTimer('00:00:00.000');
+	}
+
+	this.displayingTimer = function (string) {
+		self.elements.clockface.innerHTML = string;
+	};
+
+	this.clearSplitList = function () {
+		self.counter(0);
+		self.elements.timerList.innerHTML = '';
+	};
+
+	this.addSplit = function (label) {
+		var p = document.createElement('p');
+		var str = self.counter() + ' ' + label + ': ' + self.elements.clockface.innerHTML;
+		p.innerHTML = str;
+		self.elements.timerList.appendChild(p);
+	}
+}//View
+
+function Control(timer, view) {
+	var self = this;
+
+	var startPauseButton = view.elements.startPauseButton;
+	var splitButton      = view.elements.splitButton;
+	var clearButton      = view.elements.clearButton;
+
+	startPauseButton.addEventListener('click', startPauseClick);
+	splitButton.addEventListener('click', splitButtonClick);
+	clearButton.addEventListener('click', resetButtonClick);
+
+	function startPauseClick(){
+		if (startPauseButton.innerHTML === 'Start') {
+			timer.start(view.displayingTimer);
+			view.start();
+		}else {
+			timer.stop();
+			view.stop();
+		}
+	}//startPauseClick
+
+	function splitButtonClick(){
+		if (timer.timerName) view.addSplit('Split');
+	}
+
+	function resetButtonClick() {
+		timer.reset();
+		view.reset();
+	}//splitButtonClick
+}//Control
+
+function Counter() {
+	var count = 1;
+	return function(num){
+		count = num !== undefined ? num : count;
+		return count++;
 	}
 }
-
-function clearButtonClick() {
-	pause = 0;
-	clearInterval(timerEvent);
-	timer.reset();
-	printTimer(clockface, timer);
-	startPauseButton.innerHTML = START_BUTTON_TEXT;
-}
-
-function timerRun(){
-	interval(timeStart, Date.now(), timer);
-	printTimer(clockface, timer);
-}
-
-function interval(startTime, endTime, timer) {
-	var interval = Date.now() - timeStart + pause;
-	var H = 60 * 60 * 1000;
-	var M = 60 * 1000;
-	var S = 1000;
-
-	timer.hour = (interval / H) >> 0;
-	interval = interval % H;
-
-	timer.min = (interval / M) >> 0;
-	interval = interval % M;
-
-	timer.sec = (interval / S) >> 0;
-	interval = interval % S;
-
-	timer.msec = interval;
-	return timer;
-}
-
-function printTimer(element, timer){
-	var outString = '';
-	
-	if (timer.hour < 10) outString += '0' + timer.hour
-	else outString += timer.hour + '';
-	outString += ':';
-	
-	if (timer.min < 10) outString += '0' + timer.min
-	else outString += timer.min + '';
-	outString += ':';
-
-	if (timer.sec < 10) outString += '0' + timer.sec
-	else outString += timer.sec + '';
-	outString += '.';
-
-	if (timer.msec > 99) outString += timer.msec + '';
-	if ((timer.msec < 100)&&(timer.msec > 9)) outString += '0' + timer.msec;
-	if (timer.msec < 10) outString += '00' + timer.msec;
-
-	element.innerHTML = outString;
-}
-
-function Timer(){
-	this.hour = 0;
-	this.min  = 0;
-	this.sec  = 0;
-	this.msec = 0;
-	this.reset = function(){
-		this.hour = 0;
-		this.min  = 0;
-		this.sec  = 0;
-		this.msec = 0;
-	};
-	this.hourInc = function(){
-		++this.hour;
-		if (this.hour > 23) {
-			this.hour = 0;
-		}
-	};
-	this.minInc = function(){
-		++this.min;
-		if (this.min > 59) {
-			this.min = 0;
-			this.hourInc();
-		}
-	};
-	this.secInc = function(){
-		++this.sec;
-		if (this.sec > 59) {
-			this.sec = 0;
-			this.minInc();
-		}
-	};
-	this.msecInc = function(){
-		if (arguments.length > 0) this.msec += arguments[0]
-		else ++this.msec;
-		if (this.msec > 999) {
-			this.msec = 0;
-			this.secInc();
-		}
-	};
-};
-}());

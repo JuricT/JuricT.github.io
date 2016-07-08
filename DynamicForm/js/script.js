c8b224833af42ae7d96bfdc6c235cae6561db1ac
Juric Talovirov <jurictalovirov@gmail.com>
2016-07-09 00:37:50 +0300

    url

--- DynamicForm/js/script.js
+++ DynamicForm/js/script.js
@@ -1,24 +1,18 @@
 $(document).ready(function(e) {
-	var title = [], url = [];
+
 	var selectLang = [];
 	var select;
 
 	$("body select").each(function () {
 		$.each(this, function( index, value ) {
-			var $that = $(this)
-			url.push($that.data('url'));
-			title.push($that.data('title'));
-			selectLang.push({'title' : $that.data('title'), 'url' : $that.data('url')});
+			var $that = $(this);
+
+			selectLang.push({'title' : $that.data('title'), 'url' : $that.attr('value')});
 			if ($that.attr('selected')) {
 				select = $that.data('title');
 			}
 		});
 	});
-			console.log(selectLang);
-
-	// $('[name = frmdata]').on('click', function (e) {
-	// 	console.log(e);
-	// });
 
 	//iclude Jquery Image Dropdown v 3.5.2 
 	try {
@@ -36,7 +30,11 @@ $(document).ready(function(e) {
 			selectLang.forEach(function (item, i, arr) {
 				if (item.title === title) { 
 					select = title;
-					window.location = item.url;
+					var protocol = window.location.protocol,
+							hostname = window.location.hostname,
+							pathname = window.location.pathname;
+
+					window.location = protocol + '//' + item.url + '.' + hostname + pathname;
 				}
 			});
 		}

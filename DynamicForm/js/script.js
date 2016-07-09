174e2337453daadff5581b4d47eded6de49e0856
Juric Talovirov <jurictalovirov@gmail.com>
2016-07-09 17:11:35 +0300

    plagin localStorage

--- DynamicForm/js/script.js
+++ DynamicForm/js/script.js
@@ -1,46 +1,11 @@
 $(document).ready(function(e) {
 
-	var selectLang = [];
-	var select;
-
-	$("body select").each(function () {
-		$.each(this, function( index, value ) {
-			var $that = $(this);
-
-			// selectLang.push({'title' : $that.data('title'), 'url' : $that.attr('value')});
-			selectLang.push({'title' : $that.data('title'), 'url' : $that.data('url')});
-			if ($that.attr('selected')) {
-				select = $that.data('title');
-			}
-		});
-	});
-
 	//iclude Jquery Image Dropdown v 3.5.2 
 	try {
-		$("body select").msDropDown();
+		$("body select").languages().msDropDown();
+		$('li').languages();
 	} catch(e) {
 		alert(e.message);
 	};
-
-
-
-	$('li').on('click', function (e) {
-		var title = $(e.currentTarget).attr('title');
-
-		if ((select !== title)&&($(e.currentTarget).hasClass('selected'))) {
-			selectLang.forEach(function (item, i, arr) {
-				if (item.title === title) { 
-					select = title;
-					window.location = item.url;
-					// var protocol = window.location.protocol,
-					// 		hostname = window.location.hostname,
-					// 		pathname = window.location.pathname;
-
-					// window.location = protocol + '//' + item.url + '.' + hostname + pathname;
-
-				}
-			});
-		}
-	});
 
 });

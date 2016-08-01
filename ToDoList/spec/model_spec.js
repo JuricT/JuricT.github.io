af6295188eff6bc83430d0abe0ca9f8589006016
Juric Talovirov <jurictalovirov@gmail.com>
2016-08-01 03:43:51 +0300

    minor fixes

--- ToDoList/spec/model_spec.js
+++ ToDoList/spec/model_spec.js
@@ -1,15 +1,9 @@
 import {Model} from '../js/app/model.js';
-// var metod = require('../js/app/model.js');
-// import Model from '../js/app/model.js';
 
 describe('Class Model ', () => {
-  // var model = new Model();
   describe('Metod addItem ', () =>{
-    it('должен добавлять не пустую строку в массив data', () => {
-      // console.log(model.data);
+    it('should not add an empty string to the array data', () => {
       expect(true).toBe(true);
     });
-
-  });//addItem
-
-});//Model
+  });
+});

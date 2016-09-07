a4dcf57c80b30386944f2432a742fd6c55ac05dd
Juric Talovirov <jurictalovirov@gmail.com>
2016-09-07 20:43:18 +0300

    refactoring code (widget architecture)

--- Timer/bonus/script.js
+++ Timer/bonus/script.js
@@ -1,153 +1,212 @@
 ;(function(){
-var timer = new Timer();
-var timerContainer = document.querySelector('.timer');
-var clockface = timerContainer.querySelector('.timer-clockface');
-var timerList = timerContainer.querySelector('.timer-list');
-var startPauseButton = document.querySelector('.timer-startStop');
-var splitButton      = document.querySelector('.timer-Split');
-var clearButton      = document.querySelector('.timer-Reset');
-var timerEvent;
-var timeStart;
-var pause = 0;
+  'use strict';
+  
+  var t1 = new Timer({
+    elem: document.getElementById('timer'),
+    clockface: document.getElementsByClassName('timer-clockface')[0],
+    startStopBtn: document.getElementsByClassName('timer-startStop')[0],
+    splitBtn: document.getElementsByClassName('timer-Split')[0],
+    resetBtn: document.getElementsByClassName('timer-Reset')[0],
+    splitList: document.getElementsByClassName('timer-list')[0]
+  });
+  
+  function Timer(options) {
 
-startPauseButton.addEventListener('click', startPauseClick);
-splitButton.addEventListener('click', splitButtonClick);
-clearButton.addEventListener('click', resetButtonClick);
+    this.elements = {
+      erapper: options.elem,
+      clockface: options.clockface,
+      startStopBtn: options.startStopBtn,
+      splitBtn: options.splitBtn,
+      resetBtn: options.resetBtn,
+      splitList: options.splitList
+    };
+    
+    var that = this;
+    
+    this.model = {
+      const:{
+        h: 60 * 60 * 1000,
+        m: 60 * 1000,
+        s: 1000,
+      },
+      
+      timeReset: function() {
+        this.start = this.pause = null;
+      },
+      
+      initTime: function(){     
+        this.start = Date.now();
+        this.pause = this.pause || 0;
+      },
+      
+      frozen: function() {
+        this.pause = Date.now() - this.start + this.pause;
+      },
+      
+      interval: function() {
+        if (!('start' in this) || (!(this.start > 0))) return 0;
+        this.pause = this.pause || 0;
+        
+        return Date.now() - this.start + this.pause;
+      },
+      
+      getHour: function() {
+        var interval = this.interval();
+        
+        if (interval === 0) return 0;
+        
+        interval = this.interval();
+        
+        return (interval / this.const.h) >> 0;
+      },
+      
+      getMin: function() {
+        var interval = this.interval();
+        
+        if (interval === 0) return 0;
+        
+        interval = interval % this.const.h;
 
-// ==============================
-// ========   Events   ==========
+        return (interval / this.const.m) >> 0; 
+      },
+      
+      getSec: function() {
+        var interval = this.interval();
+        
+        interval = interval % this.const.h;
+        interval = interval % this.const.m;
 
-function startPauseClick(){
-	if (startPauseButton.innerHTML === 'Start') {
-		timer.reset();
-		timeStart = Date.now();
-		timerEvent = setInterval(timerRun, 1);
-		startPauseButton.innerHTML = 'Stop';
-	}else {
-		pause = Date.now() - timeStart + pause;
-		clearInterval(timerEvent);
-		startPauseButton.innerHTML = 'Start';
-		addSplits('Stop');
-	}
-}
+        return (interval / this.const.s) >> 0; 
+      },
+      
+      getMsec: function(interval) {
+        var interval = this.interval();
+        
+        if (interval === 0) return 0;
+        
+        interval = interval % this.const.h;
+        interval = interval % this.const.m;
+        interval = interval % this.const.s;
 
-function splitButtonClick(){
-	addSplits('Split');
-}
-
-function resetButtonClick() {
-	clearInterval(timerEvent);
-	timer.reset();
-	printTimer(clockface, timer);
-	startPauseButton.innerHTML = 'Start';
-	//очищаем список
-	pause = 0;
-	counter(0);
-	timerList.innerHTML = '';
-}
-
-// =============================
-// ======== Functions ==========
-
-function addSplits(label){
-	var p = document.createElement('p');
-	var str = counter() + ' ' + label + ': ' + clockface.innerHTML;
-	p.innerHTML = str;
-	timerList.appendChild(p);
-
-}
-
-//The counter records time listed serif.
-var counter = (function(){
-	var count = 1;
-	return function(num){
-		count = num !== undefined ? num : count;
-		return count++;
-	}
-}());
-
-
-
-function timerRun(){
-	var interval = Date.now() - timeStart + pause;
-	var H = 60 * 60 * 1000;
-	var M = 60 * 1000;
-	var S = 1000;
-
-	timer.hour = (interval / H) >> 0;
-	interval = interval % H;
-
-	timer.min = (interval / M) >> 0;
-	interval = interval % M;
-
-	timer.sec = (interval / S) >> 0;
-	interval = interval % S;
-
-	timer.msec = interval;
-	printTimer(clockface, timer);
-}
-
-
-function printTimer(element, timer){
-	var outString = '';
+        return interval; 
+      },
+      
+      getTime: function() {
+        var time = this;
+        
+        var hour = time.getHour();
+        var min = time.getMin();
+        var sec = time.getSec();
+        var msec = time.getMsec();
+        
+        var outString = '';
 	
-	if (timer.hour < 10) outString += '0' + timer.hour
-	else outString += timer.hour + '';
-	outString += ':';
-	
-	if (timer.min < 10) outString += '0' + timer.min
-	else outString += timer.min + '';
-	outString += ':';
+        if (hour < 10) outString += '0' + hour
+        else outString += hour + '';
+        outString += ':';
 
-	if (timer.sec < 10) outString += '0' + timer.sec
-	else outString += timer.sec + '';
-	outString += '.';
+        if (min < 10) outString += '0' + min
+        else outString += min + '';
+        outString += ':';
 
-	if (timer.msec > 99) outString += timer.msec + '';
-	if ((timer.msec < 100)&&(timer.msec > 9)) outString += '0' + timer.msec;
-	if (timer.msec < 10) outString += '00' + timer.msec;
+        if (sec < 10) outString += '0' + sec
+        else outString += sec + '';
+        outString += '.';
 
-	element.innerHTML = outString;
-}
+        if (msec > 99) outString += msec + '';
+        if ((msec < 100)&&(msec > 9)) outString += '0' + msec;
+        if (msec < 10) outString += '00' + msec;
 
-function Timer(){
-	this.hour = 0;
-	this.min  = 0;
-	this.sec  = 0;
-	this.msec = 0;
-	this.reset = function(){
-		this.hour = 0;
-		this.min  = 0;
-		this.sec  = 0;
-		this.msec = 0;
-	};
-	this.hourInc = function(){
-		++this.hour;
-		if (this.hour > 23) {
-			this.hour = 0;
-		}
-	};
-	this.minInc = function(){
-		++this.min;
-		if (this.min > 59) {
-			this.min = 0;
-			this.hourInc();
-		}
-	};
-	this.secInc = function(){
-		++this.sec;
-		if (this.sec > 59) {
-			this.sec = 0;
-			this.minInc();
-		}
-	};
-	this.msecInc = function(){
-		if (arguments.length > 0) this.msec += arguments[0]
-		else ++this.msec;
-		if (this.msec > 999) {
-			this.msec = 0;
-			this.secInc();
-		}
-	};
-};
+        return outString;
+      }
+    };
+    
+    this.view = {      
+      showTime: function() {
+        that.elements.clockface.innerHTML = that.model.getTime();
+      },
+      
+      trigStartStopBtn: function() {
+        if (that.elements.startStopBtn.innerHTML === 'Start') {
+          that.elements.startStopBtn.innerHTML = 'Stop';
+        } else {
+          that.elements.startStopBtn.innerHTML = 'Start';
+        };
+      },
+      
+      clear: function() {
+        that.elements.startStopBtn.innerHTML = 'Start';
+        that.elements.clockface.innerHTML = '00:00:00.000';
+        that.elements.splitList.innerHTML = '';
+      },
+      
+      addSplits: function(label) {
+        var p = document.createElement('p');
+        var str = 1 + ' ' + label + ': ' + that.elements.clockface.innerHTML;
+        p.innerHTML = str;
+        that.elements.splitList.appendChild(p);
+      }
+    };
+    
+    this.controller = {
+      init: function() {
+        that.elements.startStopBtn.addEventListener('click', function(){
+          that.controller.startPause();
+        });
+        that.elements.splitBtn.addEventListener('click', function(){
+          that.controller.split();
+        });
+        that.elements.resetBtn.addEventListener('click', function(){
+          that.controller.reset();
+        });
+      },
+      
+      startPause: function() {
+        
+        if (that.elements.startStopBtn.innerHTML === 'Start') {
+          that.controller.start();
+          that.view.trigStartStopBtn();
+        } else {
+          that.controller.stop();
+          that.view.trigStartStopBtn();
+        }
+      },
+      
+      split: function() {
+        that.view.addSplits('qqq');
+      },
+      
+      reset: function() {
+        this.clearInt();
+        that.model.timeReset();
+        that.view.clear();
+      },
+      
+      labelInterval: '',
+      
+      start: function() {
+        that.model.initTime();
+        this.setInt();
+      },
+      
+      stop: function() {
+        that.model.frozen();
+        this.clearInt();
+      },
+      
+      setInt: function() {
+        this.labelInterval = this.labelInterval || setInterval(that.view.showTime, 1);
+      },
+      
+      clearInt: function() {
+        clearInterval(this.labelInterval);
+        this.labelInterval = '';
+      }
+
+    };
+
+    that.controller.init();
+
+  }
+  
 }());

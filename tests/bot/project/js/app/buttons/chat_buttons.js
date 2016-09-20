1c646b3a3d96dae098ad02a68221987b2eccfd7b
Juric Talovirov <jurictalovirov@gmail.com>
2016-09-20 22:38:11 +0300

    Create chat_buttons.js

---
+++ tests/bot/project/js/app/buttons/chat_buttons.js
@@ -0,0 +1,90 @@
+define(
+  'buttons/chat_buttons',
+  ['helpers', 'jquery'],
+  function (helpers) {
+    'use strict';
+
+    function ChatBtn(config) {
+      var that = this;
+
+      var defaultTarget = $('.chat-btn');
+
+      var defaultSettings = {
+        states: [
+          {
+            title: 'Open',
+            callback: null
+          },
+          {
+            title: 'Close',
+            callback: null
+          },
+        ],
+
+        elements: {
+          target: defaultTarget,
+          // parent: defaultTarget.parent()
+        }
+      };
+
+      var options = $.extend(defaultSettings, config);
+
+      this._elements = {
+        target: options.elements.target,
+        // parent: options.elements.parent
+      };
+
+      this._currenState = 0;
+
+      this._states = options.states;
+
+      this.initClick();
+      this.initState();
+    }
+
+
+
+    ChatBtn.prototype.initClick = function(callbck) {
+      var that = this;
+
+      if ('parent' in (this._elements || {})){
+        this._elements.parent.on('click', callback);
+      } else {
+        this._elements.target.on('click', callback);
+      }
+
+      function callback(e) {
+        if (that._elements.target[0] === e.target) {
+          var $target = $(e.target);
+
+
+          if (helpers.getClass(callbck) === 'Function') {
+            callbck();
+          }
+
+          if (helpers.getClass(that._states[that._currenState].callback) === 'Function') {
+            that._states[that._currenState].callback();
+          }
+
+          that.nextState();
+        }
+      }
+    };
+
+    ChatBtn.prototype.initState = function () {
+      this._elements.target.text(this._states[this._currenState].title);
+    };
+
+    ChatBtn.prototype.nextState = function () {
+      this._incState();
+      this.initState();
+    };
+
+    ChatBtn.prototype._incState = function () {
+      this._currenState = (this._currenState < this._states.length - 1) ?
+      ++this._currenState : 0;
+    };
+
+    return ChatBtn;
+  }
+);

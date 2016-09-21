91d74f54bc9e5f6dc09b06f77b3a5d41672541df
Juric Talovirov <jurictalovirov@gmail.com>
2016-09-21 16:30:34 +0300

    add chatBtn, add window resize

--- tests/bot/project/js/app/controller.js
+++ tests/bot/project/js/app/controller.js
@@ -9,6 +9,18 @@ define(
       that._model = model;
       that._view = view;
 
+      view.chatBtn.states[0].callback = function() {
+        view.win.show();
+      };
+
+      view.chatBtn.states[1].callback = function() {
+        view.win.hide();
+      };
+
+      $(window).resize(function(e) {
+        if (view.win.$wrapper.is(':visible')) view.win.refreshPosition();
+      });
+
     }
 
   return Controller;

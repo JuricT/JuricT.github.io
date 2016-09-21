644c1537a492562d4879ab9a803b05dd510873c8
Juric Talovirov <jurictalovirov@gmail.com>
2016-09-21 15:15:09 +0300

    create

---
+++ tests/bot/project/js/app/template.js
@@ -0,0 +1,20 @@
+define(
+  'app/template',
+  ['jquery', 'tmpl'],
+  function () {
+    'use strict';
+
+    function Template(querySelector, element) {
+        this._querySelector = querySelector;
+        this._element = element || document.body;
+      }
+
+      Template.prototype.render = function(data) {
+        var list = tmpl($(this._querySelector).html(), {data: data});
+
+        this._element.html(list);
+      };
+
+    return Template;
+  }
+);
